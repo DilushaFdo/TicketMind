8f4de1ecc7c93da89de8e3fa0812683b1e31f6e1
DilushaFdo <130301311+DilushaFdo@users.noreply.github.com>
2026-07-08 13:50:16 +0530

    initial commit

---
+++ src/main/java/com/dilusha/TicketMind/TicketMindApplication.java
@@ -0,0 +1,13 @@
+package com.dilusha.TicketMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMindApplication.class, args);
+	}
+
+}
